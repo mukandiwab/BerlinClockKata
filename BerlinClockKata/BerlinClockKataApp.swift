ae292efe3ca4b76298e4372ba0948e0174e4cfd3
bmukandiwa <briamukk@gmal.com>
2024-01-16 16:29:47 +0100

    Initial commit

---
+++ BerlinClockKata/BerlinClockKataApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerlinClockKataApp.swift
+//  BerlinClockKata
+//
+//  Created by bmukandiwa on 16/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct BerlinClockKataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
